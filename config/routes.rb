58b25efead535d06c6aa9be0c2cbe8704e9bf3b3
piro1214 <akane.hirose.ocha@gmail.com>
2018-10-13 06:22:14 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/info'
+  resources :ideas
+  get "pages/info"
+  root to: "ideas#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
